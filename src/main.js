964f0b8c072d30683124c83af39381b358face0d
matanel7 <you@example.com>
2026-02-02 16:05:56 +0200

    Initial commit: Vite SPA boilerplate

---
+++ src/main.js
@@ -0,0 +1,9 @@
+import './style.css'
+
+const root = document.getElementById('app')
+root.innerHTML = `
+  <main>
+    <h1>Vite SPA Boilerplate</h1>
+    <p>Edit <code>src/main.js</code> and enjoy fast HMR with Vite.</p>
+  </main>
+`
